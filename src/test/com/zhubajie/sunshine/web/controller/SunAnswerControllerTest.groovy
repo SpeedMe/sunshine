58e6331badf36dae73dbb7e31f059327383af691
HuangLei <leihuang.org@gmail.com>
2015-08-31 23:24:26 +0800

    spock unit test

---
+++ src/test/com/zhubajie/sunshine/web/controller/SunAnswerControllerTest.groovy
@@ -0,0 +1,12 @@
+package com.zhubajie.sunshine.web.controller
+
+import spock.lang.Specification
+
+/**
+ * Created by simle on 2015/8/31.
+ */
+class SunAnswerControllerTest extends Specification {
+    def "GetAnswerById"() {
+
+    }
+}

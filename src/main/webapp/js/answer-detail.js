054a36bdf4628314572a05949be529d55e840fa1
HuangLei <leihuang.org@gmail.com>
2015-09-03 22:35:44 +0800

    回复信息和私聊集成velocity模板

--- src/main/webapp/js/answer-detail.js
+++ src/main/webapp/js/answer-detail.js
@@ -2,26 +2,62 @@
     var answer = {
         _param: {},
         _init: function () {
+            //回复话题的用户
             var uid = $(".topic-header").attr("data-user-id");
             var aid = $(".topic-header").attr("data-answer-id");
-            answer._event(uid, aid);
+
+            //当前登陆的用户
+            var loginUser = JSON.parse(answer._loginState()).data;
+
+            answer._hasFollowed(aid, loginUser);
+            answer._event(uid, aid, loginUser);
+
         },
-        _event: function (uid, aid) {
+        _event: function (uid, aid, loginUser) {
             //绑定返回
             $('.turn-back').on('click', function (e) {
                 history.go(-1);
             });
             //绑定私聊
             $('.bottom-btn .chat').on('click', function (data) {
-                var loc = answer._param;
-                location.href = 'chat.html?sendUserId=' + loc.userId + '&receiveUserId=' + loc.answer.data.userId;
+                //var chatData = JSON.parse(answer._loginState());
+                location.href = '/chat.html?sendUserId=' + loginUser.userId + '&receiveUserId=' + uid;
             });
             //绑定感谢
             $('.bottom-btn .thanks').on('click', function (e) {
-                $.post('/shine/sunAnswer/thankAnswer', {userId: uid, topicAnswerId: aid}, function (data) {
-                    $('.bottom-btn .thanks').off();
-                    $('.bottom-btn .thanks').css('color', '#aaa');
+                $.ajax({
+                    type: "POST",
+                    url: "/shine/sunAnswer/thankAnswer",
+                    data: {userId: uid, topicAnswerId: aid},
+                    success: function () {
+                        $('.bottom-btn .thanks').off();
+                        $('.bottom-btn .thanks').css('color', '#aaa');
+                    }
                 });
+            });
+        },
+        _loginState: function () {
+            //get函数是返回的数据其他函数接受不到因为异步原因，所以使用这种方式
+            return $.ajax({
+                type: "GET",
+                url: "/shine/sunUser/loginState",
+                async: false,
+                cache: false,
+                success: function () {
+                }
+            }).responseText;
+        },
+        _hasFollowed: function(aid, loginUser){
+            $.ajax({
+                type: "POST",
+                url: "/shine/sunAnswer/hasThankedAnswer",
+                data: {userId: loginUser.userId, topicAnswerId: aid},
+                success: function(res){
+                    if(res.data){
+                        $('.bottom-btn .thanks').off();
+                        $('.bottom-btn .thanks').css('color', '#aaa');
+                    }
+                }
             });
         }
     };
--- src/main/webapp/js/topic-detail.js
+++ src/main/webapp/js/topic-detail.js
@@ -49,80 +49,9 @@
             });
             //返回
             $('.turn-back').on('click', function (e) {
-                location.href = "topic.html?channelId=" + topic._param.channelId;
+                history.go(-1);
             });
         },
-        //_pageLoad : function(){
-        //	var loc = topic._param;
-        //	loc.href = location.href;
-        //	var sp = loc.href.lastIndexOf('topicId=');
-        //	loc.topicId = loc.href.substring(sp+8);
-        //	var tsp = loc.href.lastIndexOf('channelId='),
-        //		mid = loc.href.lastIndexOf('&');
-        //	loc.channelId = loc.href.substring(tsp+10,mid);
-        //
-        //	$.get('/shine/sunTopic/getTopicByTopicId/'+loc.topicId,function(data){
-        //		loc.topic = data;
-        //		if(loc.topic.meta.code === 200){
-        //			var locdata = loc.topic.data;
-        //			$('.content .writer-info .temperature-small').text(locdata.topicTemp+'° ');
-        //			$('.content .content-detail').text(locdata.topicContent);
-        //			topic._loadWriter(locdata.userId);
-        //		}
-        //	});
-        //
-        //	this._loadAnswer(loc.topicId);
-        //},
-        //_loadAnswer : function(tid){
-        //	var loc = topic._param,
-        //	target = $('.answer');
-        //	target.children().remove();
-        //	$.get('/shine/sunAnswer/queryAnswersByTopicIdOrderByTemp/'+tid,function(data){
-        //		loc.answers = data;
-        //		if(loc.answers.meta.code === 200){
-        //			var locdata = loc.answers.data;
-        //			for(var idx in locdata){
-        //				var html = '<li data-answerId="'+locdata[idx].topicAnswerId+'">'
-        //				            +'    <div class="fl answer-user">'
-        //				            +'        <p><img class="user-image" src="http://placehold.it/25x25" alt=""></p>'
-        //				            +'        <p class="temperature-small">'+locdata[idx].topicAnswerTemp+'° </p>'
-        //				            +'    </div>'
-        //				            +'    <div class="topic-answer">'
-        //				            +'        <p class="answer-name">匿名</p>'
-        //				            +'        <p class="answer-container">'+locdata[idx].topicAnswerContent+'</p>'
-        //				            +'    </div>'
-        //				            +'</li>';
-        //				var $tar = $(html);
-        //				target.append($tar);
-        //				if(!locdata[idx].isAnonymity){
-        //					topic._loadAnswerDetail( locdata[idx].userId,$tar);
-        //				}
-        //			}
-        //		}
-        //	})
-        //},
-        //_loadWriter : function(uid){
-        //	var loc = topic._param;
-        //	$.get('/shine/sunUser/user/'+uid,function(data){
-        //		loc.writeUser = data;
-        //		if(loc.writeUser.meta.code === 200){
-        //			var locdata = loc.writeUser.data;
-        //			$('.writer-name').text("来自"+locdata.nickname);
-        //			$('.writer-name+img').attr('src',locdata.avatarPath);
-        //		}
-        //	});
-        //},
-        //_loadAnswerDetail : function(uid, tar){
-        //	var loc = topic._param;
-        //	$.get('/shine/sunUser/user/'+uid,function(data){
-        //		loc.answerUser = data;
-        //		if(loc.answerUser.meta.code === 200){
-        //			var locdata = loc.answerUser.data;
-        //			tar.find('.answer-name').text(locdata.nickname);
-        //			tar.find('.answer-user .user-image').attr('src',locdata.avatarPath);
-        //		}
-        //	});
-        //},
         _topicFollow: function (flag, uid, tid) {
             var url = flag ? '/shine/sunTopic/followTopic' : '/shine/sunTopic/unfollowTopic';
             $.post(url, {userId: uid, topicId: tid}, function (data) {
